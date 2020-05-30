6b122edbbac7903d8dd93d2f8fd4768de718a7dc
Jake Kravas <jakekravas7@gmail.com>
2020-05-30 04:06:07 -0700

    created Url model and connected route files to server.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect to database
+connectDB();
+
 app.use(express.json({ extended: false }));
+
+// Define routes
+app.use("/", require("./routes/index"));
+app.use("/api/url", require("./routes/url"));
 
 const PORT = process.env.PORT || 5000;
 
